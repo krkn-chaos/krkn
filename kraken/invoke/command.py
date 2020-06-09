c556cd7f05cdb6aa19d79abf518028837c74b9be
Paige Rubendall <prubenda@redhat.com>
2020-06-09 17:40:22 +0000

    Some updates to node actions

--- kraken/invoke/command.py
+++ kraken/invoke/command.py
@@ -4,12 +4,12 @@ import logging
 
 # Invokes a given command and returns the stdout
 def invoke(command):
-    logging.info('Try invoking')
+    logging.info('Try invoking ' + command)
     try:
-        output = subprocess.check_output(command, shell=True,
-                                  universal_newlines=True, stderr=subprocess.STDOUT)
-        #err = output.communicate()
-        logging.info( "\n\n\nError " + str(output))
+        output = subprocess.Popen(command, shell=True,
+                                  universal_newlines=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        (out, err) = output.communicate()
+        logging.info( "Output " + str(out) + "\n\n\nError " + str(err))
     except Exception as e:
         logging.error("Failed to run %s" % (e))
     return output
@@ -18,5 +18,5 @@ def invoke(command):
 # Invoke oc debug with command
 def invoke_debug_helper(node_name, command):
 
-    return invoke("oc debug node/" + node_name + " -- chroot /host;" + command + ";")
+    return invoke("oc debug node/" + node_name + ' -- "chroot /host; ' + command + '"')
 
