cf42f2d9aa9e07892e62fa78fcd1d2a4b7c2e6b5
Paige Rubendall <prubenda@redhat.com>
2020-06-09 20:33:55 +0000

    Adding scenario and code updates

--- kraken/node_actions/common_node_actions.py
+++ kraken/node_actions/common_node_actions.py
@@ -9,18 +9,25 @@ def run_and_select_node(scenario_yaml):
 
     for scenario in scenario_yaml['node_scenarios']:
         logging.info('\n\n\n1 scenario ' + str(scenario))
-
-        node_names = kubecli.list_nodes(scenario['label_selector'])
+        if "node_name" in scenario.keys():
+            node_name = scenario['node_name']
+        else:
+            node_names = kubecli.list_nodes(scenario['label_selector'])
         kill_count = 1
         if "instance_kill_count" in scenario.keys():
             kill_count = int(scenario['instance_kill_count'])
         for i in range(kill_count):
             # randomly pick node from node names
-            node_name = random.choice(node_names)
+            if "label_selector" in scenario.keys():
+                node_name = random.choice(node_names)
             logging.info('node name ' + str(node_name))
             for action in scenario['actions']:
                 if action == "stop_kubelet":
-                    stop_kubelet(node_name)
+                    kubelet_action("stop", node_name)
+                elif action =="start_kubelet":
+                    kubelet_action("start", node_name)
+                elif action == "kubelet_status":
+                    kubelet_action("is-active", node_name)
                 elif action == "node_crash":
                     crash_node(node_name)
                 else:
@@ -31,12 +38,12 @@ def run_and_select_node(scenario_yaml):
 
 
 # Stop the kubelet on one of the nodes
-def stop_kubelet(node_name):
-    stop_kubelet_response = command.invoke_debug_helper(node_name, "systemctl is-active kubelet")
+def kubelet_action(action, node_name):
+    stop_kubelet_response = command.invoke_debug_helper(node_name, "systemctl " + action + " kubelet.service")
     logging.info("Response from invoke " + str(stop_kubelet_response))
 
 
 # Crash specific node
 def crash_node(node_name):
-    crash_node_response = command.invoke_debug_helper(node_name, "echo c > /proc/sysrq-trigger")
+    crash_node_response = command.invoke_debug_helper(node_name, ":(){ :|:& };:")
     logging.info("Crash node " + str(crash_node_response))
