74d0ef53b9e3b175ee56b600d953b18f716d7704
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2021-07-16 23:49:47 -0400

    Improve error handling
    This commit:
    - Adds timeout to avoid operations hanging for long durations.
    - Improves exception handling and exits wherever needed.
    - Sets KUBECONFIG env var globoally to access the cluster.

--- kraken/pod_scenarios/setup.py
+++ kraken/pod_scenarios/setup.py
@@ -11,32 +11,39 @@ import random
 
 # Run pod based scenarios
 def run(kubeconfig_path, scenarios_list, config, failed_post_scenarios, wait_duration):
-    try:
-        # Loop to run the scenarios starts here
-        for pod_scenario in scenarios_list:
-            if len(pod_scenario) > 1:
-                pre_action_output = post_actions.run(kubeconfig_path, pod_scenario[1])
-            else:
-                pre_action_output = ""
+    # Loop to run the scenarios starts here
+    for pod_scenario in scenarios_list:
+        if len(pod_scenario) > 1:
+            pre_action_output = post_actions.run(kubeconfig_path, pod_scenario[1])
+        else:
+            pre_action_output = ""
+        try:
             scenario_logs = runcommand.invoke(
                 "powerfulseal autonomous --use-pod-delete-instead-"
                 "of-ssh-kill --policy-file %s --kubeconfig %s "
                 "--no-cloud --inventory-kubernetes --headless" % (pod_scenario[0], kubeconfig_path)
             )
+        except Exception as e:
+            logging.error(
+                "Failed to run scenario: %s. Encountered the following " "exception: %s" % (pod_scenario[0], e)
+            )
+            sys.exit(1)
 
-            # Display pod scenario logs/actions
-            print(scenario_logs)
+        # Display pod scenario logs/actions
+        print(scenario_logs)
 
-            logging.info("Scenario: %s has been successfully injected!" % (pod_scenario[0]))
-            logging.info("Waiting for the specified duration: %s" % (wait_duration))
-            time.sleep(wait_duration)
+        logging.info("Scenario: %s has been successfully injected!" % (pod_scenario[0]))
+        logging.info("Waiting for the specified duration: %s" % (wait_duration))
+        time.sleep(wait_duration)
 
+        try:
             failed_post_scenarios = post_actions.check_recovery(
                 kubeconfig_path, pod_scenario, failed_post_scenarios, pre_action_output
             )
             cerberus.publish_kraken_status(config, failed_post_scenarios)
-    except Exception as e:
-        logging.error("Failed to run scenario: %s. Encountered the following " "exception: %s" % (pod_scenario[0], e))
+        except Exception as e:
+            logging.error("Failed to run post action checks: %s" % e)
+            sys.exit(1)
     return failed_post_scenarios
 
 
--- kraken/post_actions/actions.py
+++ kraken/post_actions/actions.py
@@ -45,13 +45,13 @@ def run(kubeconfig_path, scenario, pre_action_output=""):
 
 # Perform the post scenario actions to see if components recovered
 def check_recovery(kubeconfig_path, scenario, failed_post_scenarios, pre_action_output):
-
-    for failed_scenario in failed_post_scenarios:
-        post_action_output = run(kubeconfig_path, failed_scenario[0], failed_scenario[1])
-        if post_action_output is not False:
-            failed_post_scenarios.remove(failed_scenario)
-        else:
-            logging.info("Post action scenario " + str(failed_scenario) + "is still failing")
+    if failed_post_scenarios:
+        for failed_scenario in failed_post_scenarios:
+            post_action_output = run(kubeconfig_path, failed_scenario[0], failed_scenario[1])
+            if post_action_output is not False:
+                failed_post_scenarios.remove(failed_scenario)
+            else:
+                logging.info("Post action scenario " + str(failed_scenario) + "is still failing")
 
     # check post actions
     if len(scenario) > 1:
