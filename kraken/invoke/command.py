c0b9cb46daa2043c2a0801703c0fd68e3b2b6296
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2021-07-16 23:49:47 -0400

    Improve error handling
    This commit:
    - Adds timeout to avoid operations hanging for long durations.
    - Improves exception handling and exits wherever needed.
    - Sets KUBECONFIG env var globoally to access the cluster.

--- kraken/invoke/command.py
+++ kraken/invoke/command.py
@@ -1,17 +1,16 @@
 import subprocess
 import logging
+import sys
 
 
 # Invokes a given command and returns the stdout
-def invoke(command):
+def invoke(command, timeout=None):
     try:
-        output = subprocess.Popen(
-            command, shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
-        )
-        (out, err) = output.communicate()
+        output = subprocess.check_output(command, shell=True, universal_newlines=True, timeout=timeout)
     except Exception as e:
         logging.error("Failed to run %s, error: %s" % (command, e))
-    return out
+        sys.exit(1)
+    return output
 
 
 def run(command):
