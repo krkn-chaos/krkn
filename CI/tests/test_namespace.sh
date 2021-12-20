c6a22e2434564037d65d7099f4c37e1ccf9795d4
Paige Rubendall <prubenda@redhat.com>
2021-12-20 09:42:37 -0500

    Adding check to be sure label or namesapce is not none first

---
+++ CI/tests/test_namespace.sh
@@ -0,0 +1,19 @@
+set -xeEo pipefail
+
+source CI/tests/common.sh
+
+trap error ERR
+trap finish EXIT
+
+function funtional_test_namespace_deletion {
+  export scenario_type="namespace_scenarios"
+  export scenario_file="-  CI/scenarios/ingress_namespace.yaml"
+  export post_config=""
+  envsubst < CI/config/common_test_config.yaml > CI/config/namespace_config.yaml
+
+  python3 run_kraken.py -c CI/config/namespace_config.yaml
+  echo $?
+  echo "Namespace scenario test: Success"
+}
+
+funtional_test_namespace_deletion
