6186a45363d2e1c0776c50ba28691688b6c6da58
svishwak <svishwak@redhat.com>
2022-03-09 01:13:10 +0530

    Modified

--- run_kraken.py
+++ run_kraken.py
@@ -47,9 +47,9 @@ def main(cfg):
             config = yaml.full_load(f)
    
     if urlparse(cfg):
-	f = requests.get(cfg)
+	    f = requests.get(cfg)
     	texts=f.text
-	config=yaml.safe_load(texts)
+	    config=yaml.safe_load(texts)
         global kubeconfig_path, wait_duration
         distribution = config["kraken"].get("distribution", "openshift")
         kubeconfig_path = config["kraken"].get("kubeconfig_path", "")
@@ -302,5 +302,5 @@ if __name__ == "__main__":
         sys.exit(1)
     else:
 	#pdb.set_trace()
-	print(options.cfg)
+	    print(options.cfg)
         main(options.cfg)
