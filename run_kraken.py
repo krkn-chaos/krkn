cd4ddbc0372c1c8fd87e92513d42f5ca5578b93b
svishwak <svishwak@redhat.com>
2022-03-15 12:52:44 +0530

    Added Comments

--- run_kraken.py
+++ run_kraken.py
@@ -38,8 +38,10 @@ def main(cfg):
     print(pyfiglet.figlet_format("kraken"))
     logging.info("Starting kraken")
 
-    # Parse and read the config
-    flag = 0
+    # flag variable to check whether cfg is a file path or a http server
+    flag = 0   
+
+    # Parse and read the config                      
     if os.path.isfile(cfg):
         flag = 1
         with open(cfg, "r") as f:
