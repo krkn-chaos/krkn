e48c65a67c1d05eeb47c5f696b528fd3966ffd5c
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2022-07-20 18:43:32 +0200

    Fixes #265: Replace Powerfulseal and introduce Wolkenwalze SDK for plugin system

---
+++ kraken/plugins/__main__.py
@@ -0,0 +1,4 @@
+from kraken.plugins import PLUGINS
+
+if __name__ == "__main__":
+    print(PLUGINS.json_schema())
--- kraken/pod_scenarios/setup.py
+++ kraken/pod_scenarios/setup.py
@@ -1,5 +1,8 @@
 import logging
-import kraken.invoke.command as runcommand
+
+from arcaflow_plugin_sdk import serialization
+from kraken.plugins import pod_plugin
+
 import kraken.cerberus.setup as cerberus
 import kraken.post_actions.actions as post_actions
 import kraken.kubernetes.client as kubecli
@@ -20,19 +23,29 @@ def run(kubeconfig_path, scenarios_list, config, failed_post_scenarios, wait_dur
         try:
             # capture start time
             start_time = int(time.time())
-            scenario_logs = runcommand.invoke(
-                "powerfulseal autonomous --use-pod-delete-instead-"
-                "of-ssh-kill --policy-file %s --kubeconfig %s "
-                "--no-cloud --inventory-kubernetes --headless" % (pod_scenario[0], kubeconfig_path)
-            )
+
+            input = serialization.load_from_file(pod_scenario)
+
+            s = pod_plugin.get_schema()
+            input_data: pod_plugin.KillPodConfig = s.unserialize_input("pod", input)
+
+            if kubeconfig_path is not None:
+                input_data.kubeconfig_path = kubeconfig_path
+
+            output_id, output_data = s.call_step("pod", input_data)
+
+            if output_id == "error":
+                data: pod_plugin.PodErrorOutput = output_data
+                logging.error("Failed to run pod scenario: {}".format(data.error))
+            else:
+                data: pod_plugin.PodSuccessOutput = output_data
+                for pod in data.pods:
+                    print("Deleted pod {} in namespace {}\n".format(pod.pod_name, pod.pod_namespace))
         except Exception as e:
             logging.error(
                 "Failed to run scenario: %s. Encountered the following " "exception: %s" % (pod_scenario[0], e)
             )
             sys.exit(1)
-
-        # Display pod scenario logs/actions
-        print(scenario_logs)
 
         logging.info("Scenario: %s has been successfully injected!" % (pod_scenario[0]))
         logging.info("Waiting for the specified duration: %s" % (wait_duration))
