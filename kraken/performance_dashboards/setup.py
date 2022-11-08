1c207538b66ab3e5e50cebb7e67afb36c355d437
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2022-11-08 15:18:59 -0500

    Use run dir instead of tmp
    This commit also logs a message to handle the exception during the
    node checks.
    
    Fixes https://github.com/redhat-chaos/krkn/issues/356, https://github.com/redhat-chaos/krkn/issues/357

--- kraken/performance_dashboards/setup.py
+++ kraken/performance_dashboards/setup.py
@@ -7,19 +7,19 @@ import sys
 # Installs a mutable grafana on the Kubernetes/OpenShift cluster and loads the performance dashboards
 def setup(repo, distribution):
     if distribution == "kubernetes":
-        command = "cd /tmp/performance-dashboards/dittybopper && ./k8s-deploy.sh"
+        command = "cd performance-dashboards/dittybopper && ./k8s-deploy.sh"
     elif distribution == "openshift":
-        command = "cd /tmp/performance-dashboards/dittybopper && ./deploy.sh"
+        command = "cd performance-dashboards/dittybopper && ./deploy.sh"
     else:
         logging.error("Provided distribution: %s is not supported" % (distribution))
         sys.exit(1)
-    delete_repo = "rm -rf /tmp/performance-dashboards || exit 0"
+    delete_repo = "rm -rf performance-dashboards || exit 0"
     logging.info("Cloning, installing mutable grafana on the cluster and loading the dashboards")
     try:
         # delete repo to clone the latest copy if exists
         subprocess.run(delete_repo, shell=True, universal_newlines=True, timeout=45)
         # clone the repo
-        git.Repo.clone_from(repo, "/tmp/performance-dashboards")
+        git.Repo.clone_from(repo, "performance-dashboards")
         # deploy performance dashboards
         subprocess.run(command, shell=True, universal_newlines=True)
     except Exception as e:
