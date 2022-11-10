5c98e18e57e8be07ce3f617bac10d2e62ec3a5ea
Tullio Sebastiani <tullio.sebastiani@x3solutions.it>
2022-11-10 11:22:13 +0100

    removed wrongly added method

--- scenarios/openshift/command_runner.py
+++ scenarios/openshift/command_runner.py
@@ -28,17 +28,3 @@ class CommandRunner:
         except Exception as e:
             logging.error("Failed to run %s, error: %s" % (cmd, e))
         return output
-
-
-
-
-def run(cmd):
-    try:
-        output = subprocess.Popen(
-            cmd, shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
-        )
-        (out, err) = output.communicate()
-    except Exception as e:
-        logging.error("Failed to run %s, error: %s" % (cmd, e))
-    return out
-
