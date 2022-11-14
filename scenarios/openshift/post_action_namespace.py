88ca73fea807ac8038c0e614a075c17aa925021c
Tullio Sebastiani <tullio.sebastiani@x3solutions.it>
2022-11-14 13:40:25 +0100

    redhat-chaos/krkn#364 post_action_namespace.py subprocess to client lib
    removed useless output

--- scenarios/openshift/post_action_namespace.py
+++ scenarios/openshift/post_action_namespace.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
 import subprocess
 import time
-
-
-def run(cmd):
-    try:
-        output = subprocess.Popen(
-            cmd, shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
-        )
-        (out, err) = output.communicate()
-    except Exception as e:
-        print("Failed to run %s, error: %s" % (cmd, e))
-    return out
-
+import logging
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
 
 i = 0
+config.load_kube_config()
+cli = client.CoreV1Api()
 while i < 100:
-    projects_active = run("oc get project | grep 'ingress' | grep -c Active").rstrip()
-    if projects_active == "3":
+    try:
+        res=cli.list_namespace()
+    except ApiException as e:
+        logging.error(
+            "Exception when calling CoreV1Api->list_namespace: %s\n" % (e))
+    projects_active=0
+    for ns in res.items:
+        if "ingress" in ns.metadata.name and ns.status.phase == "Active":
+            projects_active +=1
+    if projects_active == 3:
         break
-    i += 1
+    i +=1
     time.sleep(5)
 
-if projects_active == str(3):
+if projects_active == 3:
     print("There were 3 projects running properly")
 else:
-    print("ERROR there were " + str(projects_active) + " projects running instead of 3")
+    logging.error("ERROR there were %d projects running instead of 3" % (projects_active))
