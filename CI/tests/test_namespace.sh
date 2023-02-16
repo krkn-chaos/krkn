8b18fa8a354ef50b4910fcc86caea71d45869bf5
Tullio Sebastiani <tsebastiani@users.noreply.github.com>
2023-02-16 09:42:33 +0100

    Github Action + functional tests (no *hog tests) (#382)
    * Github Action + functional tests (no *hog tests)
    
    * changed the trigger keyword to /test
    
    * removed deprecated kill_pod scenario + added namespace to app_outage (new kill_pod)
    
    * #365: renamed ingress_namespace scenario to network_diagnostrcs
    
    * requested team filter added
    
    ---------
    
    Co-authored-by: Tullio Sebastiani <tullio.sebastiani@x3solutions.it>

--- CI/tests/test_namespace.sh
+++ CI/tests/test_namespace.sh
@@ -7,10 +7,10 @@ trap finish EXIT
 
 function funtional_test_namespace_deletion {
   export scenario_type="namespace_scenarios"
-  export scenario_file="-  CI/scenarios/ingress_namespace.yaml"
+  export scenario_file="-  CI/scenarios/network_diagnostics_namespace.yaml"
   export post_config=""
+  yq '.scenarios.[0].namespace="^openshift-network-diagnostics$"' -i CI/scenarios/network_diagnostics_namespace.yaml
   envsubst < CI/config/common_test_config.yaml > CI/config/namespace_config.yaml
-
   python3 -m coverage run -a run_kraken.py -c CI/config/namespace_config.yaml
   echo $?
   echo "Namespace scenario test: Success"
