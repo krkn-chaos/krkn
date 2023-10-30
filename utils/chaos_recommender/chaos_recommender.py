e7085352529a092939576e23d4984222030371b0
Tullio Sebastiani <tsebasti@redhat.com>
2023-10-30 10:11:54 +0100

    removed unused arguments

--- utils/chaos_recommender/chaos_recommender.py
+++ utils/chaos_recommender/chaos_recommender.py
@@ -20,14 +20,11 @@ def parse_arguments(parser):
     # command line options
     parser.add_argument("-c", "--config-file", action="store", help="Config file path")
     parser.add_argument("-o", "--options", action="store_true", help="Evaluate command line options")
-    parser.add_argument("-a", "--application", action="store", default="", help="Kubernetes application name")
     parser.add_argument("-n", "--namespace", action="store", default="", help="Kubernetes application namespace")
-    parser.add_argument("-l", "--labels", action="store", default="", help="Kubernetes application labels")
     parser.add_argument("-p", "--prometheus-endpoint", action="store", default="", help="Prometheus endpoint URI")
     parser.add_argument("-k", "--kubeconfig", action="store", default=kube_config.KUBE_CONFIG_DEFAULT_LOCATION, help="Kubeconfig path")
     parser.add_argument("-t", "--token", action="store", default="", help="Kubernetes authentication token")
     parser.add_argument("-s", "--scrape-duration", action="store", default="10m", help="Prometheus scrape duration")
-    parser.add_argument("-i", "--library", action="store",default="kraken",  help="Chaos library")
     parser.add_argument("-L", "--log-level", action="store", default="INFO", help="log level (DEBUG, INFO, WARNING, ERROR, CRITICAL")
 
     parser.add_argument("-M", "--MEM", nargs='+', action="store", default=[],
@@ -50,18 +47,15 @@ def read_configuration(config_file_path):
     with open(config_file_path, mode="r") as config_file:
         config = yaml.safe_load(config_file)
 
-    application = config.get("application", "")
     log_level = config.get("log level", "INFO")
     namespace = config.get("namespace", "")
-    labels = config.get("labels", "")
     kubeconfig = config.get("kubeconfig", kube_config.KUBE_CONFIG_DEFAULT_LOCATION)
 
     prometheus_endpoint = config.get("prometheus_endpoint", "")
     auth_token = config.get("auth_token", "")
     scrape_duration = config.get("scrape_duration", "10m")
-    chaos_library = config.get("chaos_library", "kraken")
     chaos_tests = config.get("chaos_tests" , {})
-    return (application, namespace, labels, kubeconfig, prometheus_endpoint, auth_token, scrape_duration, chaos_library,
+    return (namespace, kubeconfig, prometheus_endpoint, auth_token, scrape_duration,
             chaos_tests, log_level)
 
 def prompt_input(prompt, default_value):
@@ -80,33 +74,24 @@ def main():
         sys.exit(1)
 
     if args.config_file is not None:
-        (application,
+        (
          namespace,
-         labels,
          kubeconfig,
          prometheus_endpoint,
          auth_token,
          scrape_duration,
-         chaos_library,
          chaos_tests,
          log_level
          ) = read_configuration(args.config_file)
 
     if args.options:
-        application = args.application
         namespace = args.namespace
-        labels = args.labels
         kubeconfig = args.kubeconfig
         auth_token = args.token
         scrape_duration = args.scrape_duration
-        chaos_library = args.library
         log_level = args.log_level
         prometheus_endpoint = args.prometheus_endpoint
-        chaos_tests = {}
-        chaos_tests["MEM"] = args.MEM
-        chaos_tests["GENERIC"] = args.GENERIC
-        chaos_tests["CPU"] = args.CPU
-        chaos_tests["NETWORK"] = args.NETWORK
+        chaos_tests = {"MEM": args.MEM, "GENERIC": args.GENERIC, "CPU": args.CPU, "NETWORK": args.NETWORK}
 
     if log_level not in ["DEBUG","INFO", "WARNING", "ERROR","CRITICAL"]:
         logging.error(f"{log_level} not a valid log level")
@@ -115,13 +100,10 @@ def main():
     logging.basicConfig(level=log_level)
 
     logging.info("============================INPUTS===================================")
-    logging.info(f"Application: {application}")
     logging.info(f"Namespace: {namespace}")
-    logging.info(f"Labels: {labels}")
     logging.info(f"Kubeconfig: {kubeconfig}")
     logging.info(f"Prometheus endpoint: {prometheus_endpoint}")
     logging.info(f"Scrape duration: {scrape_duration}")
-    logging.info(f"Chaos library: {chaos_library}")
     for test in chaos_tests.keys():
         logging.info(f"Chaos tests {test}: {chaos_tests[test]}")
     logging.info("=====================================================================")
