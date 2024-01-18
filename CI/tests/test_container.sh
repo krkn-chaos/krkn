f801dfce54b171d3fc39afbda62d4a6e5bed827a
Tullio Sebastiani <tsebasti@redhat.com>
2024-01-18 16:59:18 +0100

    functional tests pointing to real scenario config files
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    typo
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    app_outage fix
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    typo
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    typo
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>

--- CI/tests/test_container.sh
+++ CI/tests/test_container.sh
@@ -8,9 +8,11 @@ trap finish EXIT
 pod_file="CI/scenarios/hello_pod.yaml"
 
 function functional_test_container_crash {
-
+  yq -i '.scenarios[0].namespace="default"' scenarios/openshift/app_outage.yaml
+  yq -i '.scenarios[0].label_selector="scenario=container"' scenarios/openshift/app_outage.yaml
+  yq -i '.scenarios[0].container_name="fedtools"' scenarios/openshift/app_outage.yaml
   export scenario_type="container_scenarios"
-  export scenario_file="- CI/scenarios/container_scenario.yml"
+  export scenario_file="- scenarios/openshift/app_outage.yaml"
   export post_config=""
   envsubst < CI/config/common_test_config.yaml > CI/config/container_config.yaml
 
--- CI/tests/test_net_chaos.sh
+++ CI/tests/test_net_chaos.sh
@@ -7,9 +7,16 @@ trap finish EXIT
 
 
 function functional_test_network_chaos {
+  yq -i '.network_chaos.duration=10' scenarios/openshift/network_chaos.yaml
+  yq -i '.network_chaos.node_name="kind-worker2"' scenarios/openshift/network_chaos.yaml
+  yq -i '.network_chaos.egress.bandwidth="100mbit"' scenarios/openshift/network_chaos.yaml
+  yq -i 'del(.network_chaos.interfaces)' scenarios/openshift/network_chaos.yaml
+  yq -i 'del(.network_chaos.label_selector)' scenarios/openshift/network_chaos.yaml
+  yq -i 'del(.network_chaos.egress.latency)' scenarios/openshift/network_chaos.yaml
+  yq -i 'del(.network_chaos.egress.loss)' scenarios/openshift/network_chaos.yaml
 
   export scenario_type="network_chaos"
-  export scenario_file="CI/scenarios/network_chaos.yaml"
+  export scenario_file="scenarios/openshift/network_chaos.yaml"
   export post_config=""
   envsubst < CI/config/common_test_config.yaml > CI/config/network_chaos.yaml
   python3 -m coverage run -a run_kraken.py -c CI/config/network_chaos.yaml
