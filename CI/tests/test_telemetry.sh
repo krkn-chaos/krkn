5d3b04f06f7d654e2440f3222f4140f51b764156
Tullio Sebastiani <tsebasti@redhat.com>
2024-03-01 11:22:27 +0100

    fixed the telemetry funtest according to the new telemetry API
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>

--- CI/tests/test_telemetry.sh
+++ CI/tests/test_telemetry.sh
@@ -15,7 +15,7 @@ function functional_test_telemetry {
   yq -i '.telemetry.enabled=True' CI/config/common_test_config.yaml
   yq -i '.telemetry.full_prometheus_backup=True' CI/config/common_test_config.yaml
   yq -i '.performance_monitoring.check_critical_alerts=True' CI/config/common_test_config.yaml
-  yq -i '.performance_monitoring.prometheus_url=http://localhost:9090' CI/config/common_test_config.yaml
+  yq -i '.performance_monitoring.prometheus_url="http://localhost:9090"' CI/config/common_test_config.yaml"
   yq -i '.telemetry.run_tag=env(RUN_TAG)' CI/config/common_test_config.yaml
 
   export scenario_type="arcaflow_scenarios"
@@ -23,7 +23,7 @@ function functional_test_telemetry {
   export post_config=""
   envsubst < CI/config/common_test_config.yaml > CI/config/telemetry.yaml
   python3 -m coverage run -a run_kraken.py -c CI/config/telemetry.yaml
-  RUN_FOLDER=`cat CI/out/test_telemetry.out | grep amazonaws.com | sed -rn "s#.*https:\/\/.*\/download/(.*)#\1#p"`
+  RUN_FOLDER=`cat CI/out/test_telemetry.out | grep amazonaws.com | sed -rn "s#.*https:\/\/.*\/files/(.*)#\1#p"`
   $AWS_CLI s3 ls "s3://$AWS_BUCKET/$RUN_FOLDER/" | awk '{ print $4 }' > s3_remote_files
   echo "checking if telemetry files are uploaded on s3"
   cat s3_remote_files | grep events-00.json || ( echo "FAILED: events-00.json not uploaded" && exit 1 )
