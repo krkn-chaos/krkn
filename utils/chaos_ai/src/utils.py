9c588e446952ab1c46b317eca89492c5ab2cfc4b
Sandeep Hans <shans001@in.ibm.com>
2024-06-20 14:03:08 +0530

    check if cluster is accessible

--- utils/chaos_ai/src/utils.py
+++ utils/chaos_ai/src/utils.py
@@ -1,4 +1,6 @@
 import re
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
 
 
 def get_load(fault):
@@ -8,3 +10,23 @@ def get_load(fault):
         load = params[0].strip('()')
         fault = fault.strip(params[0])
     return fault, load
+
+
+def is_cluster_accessible(kubeconfig_path: str) -> bool:
+    try:
+        config.load_kube_config(config_file=kubeconfig_path)
+        v1 = client.CoreV1Api()
+
+        # Try to list nodes in the cluster
+        nodes = v1.list_node()
+        print("#Nodes in Cluster: ", len(nodes.items))
+
+        return True
+    except (FileNotFoundError, ApiException, Exception) as e:
+        print(f"Cluster is not accessible: {e}")
+        return False
+
+if __name__ == '__main__':
+    # print(is_cluster_accessible("~/Downloads/chaos/kraken/kubeconfig"))
+    print(is_cluster_accessible("~/Downloads/kube-config-raw"))
+
