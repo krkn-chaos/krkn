c7e068a5627be2199c62b3291c6c63a9278c97ee
Tullio Sebastiani <tsebastiani@users.noreply.github.com>
2025-01-31 17:01:26 +0100

    Hog scenario porting from arcaflow to native (#748)
    * added new native hog scenario
    
    * removed arcaflow dependency + legacy hog scenarios
    
    * config update
    
    * changed hog configuration structure + added average samples
    
    * fix on cpu count
    
    * removes tripledes warning
    
    * changed selector format
    
    * changed selector syntax
    
    * number of nodes option
    
    * documentation
    
    * functional tests
    
    * exception handling on hog deployment thread

---
+++ CI/tests/test_memory_hog.sh
@@ -0,0 +1,19 @@
+set -xeEo pipefail
+
+source CI/tests/common.sh
+
+trap error ERR
+trap finish EXIT
+
+
+function functional_test_memory_hog {
+  yq -i '.node_selector="kubernetes.io/hostname=kind-worker2"' scenarios/kube/memory-hog.yml
+  export scenario_type="hog_scenarios"
+  export scenario_file="scenarios/kube/memory-hog.yml"
+  export post_config=""
+  envsubst < CI/config/common_test_config.yaml > CI/config/memory_hog.yaml
+  python3 -m coverage run -a run_kraken.py -c CI/config/memory_hog.yaml
+  echo "Memory Hog: Success"
+}
+
+functional_test_memory_hog
