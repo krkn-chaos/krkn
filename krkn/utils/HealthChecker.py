13813b9bb29074c77b4e387514cbe5feb33f38d4
kattameghana <meghanakatta8@gmail.com>
2025-03-11 18:29:17 +0530

    Changed the start timestamp and end timestamp data type to the datetime
    Signed-off-by: kattameghana <meghanakatta8@gmail.com>

--- krkn/utils/HealthChecker.py
+++ krkn/utils/HealthChecker.py
@@ -21,7 +21,7 @@ class HealthChecker:
 
     def run_health_check(self, health_check_config, health_check_telemetry_queue: queue.Queue):
         if health_check_config and health_check_config["config"] and any(config.get("url") for config in health_check_config["config"]):
-            health_check_start_time_stamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            health_check_start_time_stamp = datetime.now()
             health_check_telemetry = []
             health_check_tracker = {}
             interval = health_check_config["interval"] if health_check_config["interval"] else 2
@@ -40,7 +40,7 @@ class HealthChecker:
 
                     if response["status_code"] != 200:
                         if config["url"] not in health_check_tracker:
-                            start_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                            start_timestamp = datetime.now()
                             health_check_tracker[config["url"]] = {
                                 "status_code": response["status_code"],
                                 "start_timestamp": start_timestamp
@@ -49,36 +49,31 @@ class HealthChecker:
                             if config["exit_on_failure"] and config["exit_on_failure"] == True and self.ret_value==0: self.ret_value = 2
                     else:
                         if config["url"] in health_check_tracker:
-                            end_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                            end_timestamp = datetime.now()
                             start_timestamp = health_check_tracker[config["url"]]["start_timestamp"]
                             previous_status_code = str(health_check_tracker[config["url"]]["status_code"])
-
-                            start_time = datetime.strptime(start_timestamp, "%Y-%m-%d %H:%M:%S")
-                            end_time = datetime.strptime(end_timestamp, "%Y-%m-%d %H:%M:%S")
-                            duration = str(end_time - start_time)
+                            duration = (end_timestamp - start_timestamp).total_seconds()
                             downtime_record = {
                                 "url": config["url"],
                                 "status": False,
                                 "status_code": previous_status_code,
-                                "start_timestamp": start_timestamp,
-                                "end_timestamp": end_timestamp,
+                                "start_timestamp": start_timestamp.isoformat(),
+                                "end_timestamp": end_timestamp.isoformat(),
                                 "duration": duration
                             }
                             health_check_telemetry.append(downtime_record)
                             del health_check_tracker[config["url"]]
                     time.sleep(interval)
-            health_check_end_time_stamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            health_check_end_time_stamp = datetime.now()
             for url, status in response_tracker.items():
                 if status == True:
-                    start_time = datetime.strptime(health_check_start_time_stamp, "%Y-%m-%d %H:%M:%S")
-                    end_time = datetime.strptime(health_check_end_time_stamp, "%Y-%m-%d %H:%M:%S")
-                    duration = str(end_time - start_time)
+                    duration = (health_check_end_time_stamp - health_check_start_time_stamp).total_seconds()
                     success_response ={
                         "url": url,
                         "status": True,
                         "status_code": 200,
-                        "start_timestamp": health_check_start_time_stamp,
-                        "end_timestamp": health_check_end_time_stamp,
+                        "start_timestamp": health_check_start_time_stamp.isoformat(),
+                        "end_timestamp": health_check_end_time_stamp.isoformat(),
                         "duration": duration
                     }
                     health_check_telemetry.append(success_response)
