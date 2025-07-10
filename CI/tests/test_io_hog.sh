166a93fff83f3f010227d1836566e528a97a94d9
Paige Patton <prubenda@redhat.com>
2025-07-10 14:21:12 -0400

    adding details of node for hog failure
    Signed-off-by: Paige Patton <prubenda@redhat.com>

--- CI/tests/test_io_hog.sh
+++ CI/tests/test_io_hog.sh
@@ -5,12 +5,13 @@ source CI/tests/common.sh
 trap error ERR
 trap finish EXIT
 
-
 function functional_test_io_hog {
-  yq -i '.node_selector="kubernetes.io/hostname=kind-worker2"' scenarios/kube/io-hog.yml
+  yq -i '."node-selector"="kubernetes.io/hostname=kind-worker2"' scenarios/kube/io-hog.yml
   export scenario_type="hog_scenarios"
   export scenario_file="scenarios/kube/io-hog.yml"
   export post_config=""
+
+  cat $scenario_file
   envsubst < CI/config/common_test_config.yaml > CI/config/io_hog.yaml
   python3 -m coverage run -a run_kraken.py -c CI/config/io_hog.yaml
   echo "IO Hog: Success"
