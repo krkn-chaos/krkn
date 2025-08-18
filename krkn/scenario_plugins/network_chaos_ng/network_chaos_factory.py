4603a8c866f191b24af5325356955ce2fbf3e818
Tullio Sebastiani <tsebasti@redhat.com>
2025-08-18 12:08:43 +0200

    network chaos ng interface changes propagated to existing scenarios
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>

--- krkn/scenario_plugins/network_chaos_ng/network_chaos_factory.py
+++ krkn/scenario_plugins/network_chaos_ng/network_chaos_factory.py
@@ -1,17 +1,18 @@
 from krkn_lib.telemetry.ocp import KrknTelemetryOpenshift
 
-from krkn.scenario_plugins.network_chaos_ng.models import NetworkFilterConfig
+from krkn.scenario_plugins.network_chaos_ng.models import NetworkFilterConfig, PodNetworkShaping
 from krkn.scenario_plugins.network_chaos_ng.modules.abstract_network_chaos_module import (
     AbstractNetworkChaosModule,
 )
 from krkn.scenario_plugins.network_chaos_ng.modules.node_network_filter import (
     NodeNetworkFilterModule,
 )
+from krkn.scenario_plugins.network_chaos_ng.modules.pod_egress_shaping import PodEgressShapingModule
+from krkn.scenario_plugins.network_chaos_ng.modules.pod_ingress_shaping import PodIngressShapingModule
 from krkn.scenario_plugins.network_chaos_ng.modules.pod_network_filter import (
     PodNetworkFilterModule,
 )
 
-supported_modules = ["node_network_filter", "pod_network_filter"]
 
 
 class NetworkChaosFactory:
@@ -22,8 +23,6 @@ class NetworkChaosFactory:
     ) -> AbstractNetworkChaosModule:
         if config["id"] is None:
             raise Exception("network chaos id cannot be None")
-        if config["id"] not in supported_modules:
-            raise Exception(f"{config['id']} is not a supported network chaos module")
 
         if config["id"] == "node_network_filter":
             config = NetworkFilterConfig(**config)
@@ -31,9 +30,25 @@ class NetworkChaosFactory:
             if len(errors) > 0:
                 raise Exception(f"config validation errors: [{';'.join(errors)}]")
             return NodeNetworkFilterModule(config, kubecli)
-        if config["id"] == "pod_network_filter":
+        elif config["id"] == "pod_network_filter":
             config = NetworkFilterConfig(**config)
             errors = config.validate()
             if len(errors) > 0:
                 raise Exception(f"config validation errors: [{';'.join(errors)}]")
             return PodNetworkFilterModule(config, kubecli)
+
+        elif config["id"] == "pod_egress_shaping":
+            config = PodNetworkShaping(**config)
+            errors = config.validate()
+            if len(errors) > 0:
+                raise Exception(f"config validation errors: [{';'.join(errors)}]")
+            return PodEgressShapingModule(config, kubecli)
+
+        elif config["id"] == "pod_ingress_shaping":
+            config = PodNetworkShaping(**config)
+            errors = config.validate()
+            if len(errors) > 0:
+                raise Exception(f"config validation errors: [{';'.join(errors)}]")
+            return PodIngressShapingModule(config, kubecli)
+        else:
+            raise Exception(f"{config['id']} Network Chaos NG scenario type not supported (yet)!")
