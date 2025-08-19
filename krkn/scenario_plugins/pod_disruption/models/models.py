2dc633e8e754c93d3e9754cd8fa235abb82c93c7
Sahil Shah <sahshah@redhat.com>
2025-08-19 15:40:34 -0400

    Adding node_label_selector for pod scenarios
    Signed-off-by: Sahil Shah <sahshah@redhat.com>

--- krkn/scenario_plugins/pod_disruption/models/models.py
+++ krkn/scenario_plugins/pod_disruption/models/models.py
@@ -11,6 +11,7 @@ class InputParams:
             self.label_selector = config["label_selector"] if "label_selector" in config else ""
             self.namespace_pattern = config["namespace_pattern"] if "namespace_pattern" in config else ""
             self.name_pattern = config["name_pattern"] if "name_pattern" in config else ""
+            self.node_label_selector = config["node_label_selector"] if "node_label_selector" in config else ""
 
     namespace_pattern: str
     krkn_pod_recovery_time: int
@@ -19,3 +20,4 @@ class InputParams:
     kill: int
     label_selector: str
     name_pattern: str
+    node_label_selector: str
