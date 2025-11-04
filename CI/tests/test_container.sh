d9611d753137c6ce64278bbd8306dcf9ca5f9140
Paige Patton <prubenda@redhat.com>
2025-11-04 08:30:48 -0500

    adding debug option

--- CI/tests/test_container.sh
+++ CI/tests/test_container.sh
@@ -16,7 +16,7 @@ function functional_test_container_crash {
   export post_config=""
   envsubst < CI/config/common_test_config.yaml > CI/config/container_config.yaml
 
-  python3 -m coverage run -a run_kraken.py -c CI/config/container_config.yaml
+  python3 -m coverage run -a run_kraken.py -c CI/config/container_config.yaml --debug
   echo "Container scenario test: Success"
 }
 
