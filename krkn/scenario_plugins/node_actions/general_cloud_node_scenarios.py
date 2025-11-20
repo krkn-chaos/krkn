56ad391a68507962b9132ba0b924e97b7015fc39
Wei Liu <weiliu@redhat.com>
2025-11-20 13:39:23 +0800

    Add poll_interval to other cloud platforms
    Signed-off-by: Wei Liu <weiliu@redhat.com>

--- krkn/scenario_plugins/node_actions/general_cloud_node_scenarios.py
+++ krkn/scenario_plugins/node_actions/general_cloud_node_scenarios.py
@@ -18,21 +18,21 @@ class general_node_scenarios(abstract_node_scenarios):
         self.node_action_kube_check = node_action_kube_check
 
     # Node scenario to start the node
-    def node_start_scenario(self, instance_kill_count, node, timeout):
+    def node_start_scenario(self, instance_kill_count, node, timeout, poll_interval):
         logging.info(
             "Node start is not set up yet for this cloud type, "
             "no action is going to be taken"
         )
 
     # Node scenario to stop the node
-    def node_stop_scenario(self, instance_kill_count, node, timeout):
+    def node_stop_scenario(self, instance_kill_count, node, timeout, poll_interval):
         logging.info(
             "Node stop is not set up yet for this cloud type,"
             " no action is going to be taken"
         )
 
     # Node scenario to terminate the node
-    def node_termination_scenario(self, instance_kill_count, node, timeout):
+    def node_termination_scenario(self, instance_kill_count, node, timeout, poll_interval):
         logging.info(
             "Node termination is not set up yet for this cloud type, "
             "no action is going to be taken"
