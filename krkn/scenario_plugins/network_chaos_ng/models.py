c975bd72d0da87549476404019fc29c8179976d8
Tullio Sebastiani <tsebasti@redhat.com>
2025-12-11 16:15:03 +0100

    all field optional
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>

--- krkn/scenario_plugins/network_chaos_ng/models.py
+++ krkn/scenario_plugins/network_chaos_ng/models.py
@@ -1,7 +1,7 @@
 import re
 from dataclasses import dataclass
 from enum import Enum
-from typing import TypeVar
+from typing import TypeVar, Optional
 
 
 class NetworkChaosScenarioType(Enum):
@@ -64,24 +64,26 @@ class NetworkFilterConfig(BaseNetworkChaosConfig):
 
 @dataclass
 class NetworkChaosConfig(BaseNetworkChaosConfig):
-    latency: str
-    loss: str
-    bandwidth: str
-    force: bool
+    latency: Optional[str] = None
+    loss: Optional[str] = None
+    bandwidth: Optional[str] = None
+    force: Optional[bool] = None
 
     def validate(self) -> list[str]:
         errors = super().validate()
         latency_regex = re.compile(r"^(\d+)(us|ms|s)$")
         bandwidth_regex = re.compile(r"^(\d+)(bit|kbit|mbit|gbit|tbit)$")
-
-        if not (latency_regex.match(self.latency)):
-            errors.append(
-                "latency must be a number followed by `us` (microseconds) or `ms` (milliseconds), or `s` (seconds)"
-            )
-        if not (bandwidth_regex.match(self.bandwidth)):
-            errors.append(
-                "bandwidth must be a number followed by `bit` `kbit` or `mbit` or `tbit`"
-            )
-        if "%" in self.loss or not self.loss.isdigit():
-            errors.append("loss must be a number followed without the `%` symbol")
+        if self.latency:
+            if not (latency_regex.match(self.latency)):
+                errors.append(
+                    "latency must be a number followed by `us` (microseconds) or `ms` (milliseconds), or `s` (seconds)"
+                )
+        if self.bandwidth:
+            if not (bandwidth_regex.match(self.bandwidth)):
+                errors.append(
+                    "bandwidth must be a number followed by `bit` `kbit` or `mbit` or `tbit`"
+                )
+        if self.loss:
+            if "%" in self.loss or not self.loss.isdigit():
+                errors.append("loss must be a number followed without the `%` symbol")
         return errors
