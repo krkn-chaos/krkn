3fb3743657e492c84500596fb73e7484a9f54028
Arpit Raj <vrxn.arp1traj@gmail.com>
2026-03-09 16:42:04 +0000

    fix: use sorted() instead of .sort() for key validation (#1182)
    Signed-off-by: Arpit Raj <vrxn.arp1traj@gmail.com>

--- krkn/prometheus/client.py
+++ krkn/prometheus/client.py
@@ -46,7 +46,7 @@ def alerts(
             sys.exit(1)
 
         for alert in profile_yaml:
-            if list(alert.keys()).sort() != ["expr", "description", "severity"].sort():
+            if sorted(alert.keys()) != sorted(["expr", "description", "severity"]):
                 logging.error(f"wrong alert {alert}, skipping")
                 continue
 
@@ -205,8 +205,8 @@ def metrics(
                    query
                 )
             elif (
-                list(metric_query.keys()).sort()
-                == ["query", "metricName"].sort()
+                sorted(metric_query.keys())
+                == sorted(["query", "metricName"])
             ):
                 metrics_result = prom_cli.process_prom_query_in_range(
                     query,
